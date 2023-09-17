74e04fb062fa2984f76b556423e89a4f6fe52aaf
i544c <vrc@i544c.me>
2023-09-17 16:00:22 +0900

    Initial commit

---
+++ GlobalToggle/README.md
@@ -0,0 +1,28 @@
+# Global Toggle
+
+オブジェクトの表示/非表示を切り替えるためのスクリプトです。
+グローバルで同期し、後からジョインしたユーザーにも対応します。
+
+
+## 必要なもの
+
+- Lura's Switch【SDK2／SDK3／VCC】
+  - https://lura.booth.pm/items/1969082
+
+
+## 使い方
+
+1. Prefabs/GlobalToggle.prefab をシーンにドラッグします。
+2. ヒエラルキービューで GlobalToggle/ToggleButton を見つけて開きます。
+3. インスペクタービューで ToggleButton (U# Script) コンポーネントを見つけて、Objects に表示/非表示を切り替えたいオブジェクトを登録します。
+
+
+## ライセンス
+煮るなり焼くなり好きにしてください。
+使用に際し、特にクレジットの表示は必要ありません。
+
+
+## 連絡先
+
+もしなにかバグを見つけたり、追加してほしい機能があったら、こちらまでお気軽にご連絡ください。
+[@i544c@buicha.social](https://buicha.social/@i544c)
---
+++ GlobalToggle/Scripts/ToggleButton.cs
@@ -0,0 +1,59 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace i544c {
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+    public class ToggleButton : UdonSharpBehaviour
+    {
+        [Header("初期状態を設定")]
+        [UdonSynced, FieldChangeCallback(nameof(isActive))] public bool _isActive = false;
+        private bool isActive {
+            get => _isActive;
+            set {
+                _isActive = value;
+                ToggleObjects();
+            }
+        }
+
+        [Header("ここに表示/非表示を切り替えたいオブジェクトを設定")]
+        [SerializeField] private GameObject[] objects;
+
+        [Header("Element 0 に Off 状態のボタン, 1 に On 状態のボタンを設定")]
+        [SerializeField] private GameObject[] switches = new GameObject[2];
+
+        private GameObject switchOff, switchOn;
+
+        void Start()
+        {
+            switchOff = switches[0];
+            switchOn  = switches[1];
+            ToggleObjects(); // 初期状態の反映のため
+        }
+
+        public override void Interact()
+        {
+            Networking.SetOwner(Networking.LocalPlayer, gameObject); // 同期変数変更のため
+            isActive = !isActive;
+            RequestSerialization();
+        }
+
+        private void ToggleObjects()
+        {
+            switchOff.SetActive(!isActive);
+            switchOn.SetActive(isActive);
+
+            foreach (GameObject obj in objects)
+            {
+                obj.SetActive(isActive);
+            }
+        }
+
+        public override void OnPlayerJoined(VRCPlayerApi player)
+        {
+            if (Networking.LocalPlayer == player) RequestSerialization();
+        }
+    }
+}
